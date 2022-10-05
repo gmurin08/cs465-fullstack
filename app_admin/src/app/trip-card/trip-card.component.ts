9ca36ecce6fec0d75060c5f3865e43b44175dbce
Gino Murin <gmurin@gmail.com>
2022-10-05 15:50:42 -0400

    abstracted trip-card logic from parent trip-listing into dedicated component

---
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-card',
+  templateUrl: './trip-card.component.html',
+  styleUrls: ['./trip-card.component.css']
+})
+export class TripCardComponent implements OnInit {
+  /*
+  Calls trip data from parent component
+  '../trip-listing/trip-listing.component.html'
+  */
+  @Input('trip') trip: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
