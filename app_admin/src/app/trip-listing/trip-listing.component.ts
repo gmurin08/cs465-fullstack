19098c9a66cc3788bb2a1757ec37c6e255de09bb
Gino Murin <gmurin@gmail.com>
2022-10-05 17:00:49 -0400

    implemented service class to import data from mongodb

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import {trips} from '../data/trips';
+/* Previously used to render static data from file:
+import {trips} from '../data/trips'; 
+*/
+// Now using service object to pull data
+import { TripDataService } from '../services/trip-data.service';
+import { Trip } from '../models/trip';
 
 @Component({
   selector: 'app-trip-listing',
   templateUrl: './trip-listing.component.html',
-  styleUrls: ['./trip-listing.component.css']
+  styleUrls: ['./trip-listing.component.css'],
+  providers: [TripDataService]
 })
 export class TripListingComponent implements OnInit {
 
-  trips: Array<any> = trips;
+  //Init empty trip array and message string
+  trips: Trip[];
+  message: string;
   
-  constructor() { }
+  constructor(private tripDataService: TripDataService) { }
 
-  ngOnInit() {
+  //component version of getTrips that calls the service object version
+  private getTrips(): void{
+    console.log('Inside TripListingComponent #getTrips')
+    this.message = 'Searching for trips';
+    //Service object returns promise so pull data and assign to array
+    this.tripDataService.getTrips().then(foundTrips=>{
+      this.message = foundTrips.length > 0 ? '' : 'No trips found';
+      this.trips = foundTrips;
+    });
+  }
+
+  ngOnInit(): void {
+    this.getTrips();
   }
 
 }
